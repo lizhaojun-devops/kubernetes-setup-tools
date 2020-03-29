6b83cb3c8b69e05e22fd6dfbb377fd8c824daf4d
lizhaojun <lizhao_jun@aliyun.com>
2020-03-29 22:41:21 +0800

    add all

--- bin/download.sh
+++ bin/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 echo -e "\033[36m开始下载k8s组件的安装包...\033[0m"
-sh -c "$(curl -fsSL http://172.19.30.116/mirror/.help/centos_install.sh)"
-sh -c "$(curl -fsSL http://172.19.30.116/mirror/.help/epel_install.sh)"
 yum install wget ftp sshpass openssh -y >/dev/null 2>&1
 mkdir ./work
 rm -rf ./work/*
@@ -10,21 +8,20 @@ cd ./work
 source ../conf/environment.conf
 echo " "
 echo "下载kubernetes-server kubernetes-client包"
-wget -c ftp://172.19.2.252/Kubernetes/kubernetes-binary-amd64/${KUBE_VERSION}/kubernetes-server-linux-amd64.tar.gz
-wget -c ftp://172.19.2.252/Kubernetes/kubernetes-binary-amd64/${KUBE_VERSION}/kubernetes-client-linux-amd64.tar.gz
+wget -c https://storage.googleapis.com/kubernetes-release/release/${KUBE_VERSION}/kubernetes-server-linux-amd64.tar.gz
+wget -c https://storage.googleapis.com/kubernetes-release/release/${KUBE_VERSION}/kubernetes-client-linux-amd64.tar.gz
 echo "下载etcd安装包"
-wget -c ftp://172.19.2.252/Kubernetes/etcd/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
+wget -c https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
 echo "下载flannel"
-wget -c ftp://172.19.2.252/Kubernetes/flannel/flannel-${FLANNEL_VERSION}-linux-amd64.tar.gz
+wget -c https://github.com/coreos/flannel/releases/download/${FLANNEL_VERSION}/flannel-${FLANNEL_VERSION}-linux-amd64.tar.gz
 echo "下载cfssl"
-wget -c ftp://172.19.2.252/Kubernetes/cfssl/cfssl-certinfo_linux-amd64 -O cfssl-certinfo
-wget -c ftp://172.19.2.252/Kubernetes/cfssl/cfssl_linux-amd64 -O cfssl
-wget -c ftp://172.19.2.252/Kubernetes/cfssl/cfssljson_linux-amd64 -O cfssljson
+wget -c https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -O cfssl
+wget -c https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -O cfssljson
+wget -c https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 -O cfssl-certinfo
 echo "下载nginx"
-wget -c ftp://172.19.2.252/Kubernetes/nginx/nginx-${NGINX_VERSION}.tar.gz
+wget -c http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 echo "下载centos kernel"
-wget -c ftp://172.19.2.252/Kubernetes/kernel/kernel-ml-${Kernel_Version}.el7.elrepo.x86_64.rpm
-wget -c ftp://172.19.2.252/Kubernetes/kernel/kernel-ml-devel-${Kernel_Version}.el7.elrepo.x86_64.rpm
+wget -c http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml{,-devel}-${Kernel_Version}.el7.elrepo.x86_64.rpm
 echo ""
 echo "download ok！"
 echo ""
--- bin/kube-docker-install.sh
+++ bin/kube-docker-install.sh
@@ -5,21 +5,17 @@ source ./conf/environment.conf
 for node_ip in ${NODE_IPS[@]}
   do
     echo ">>> ${node_ip}"
-    ssh root@${node_ip} "wget -q -O /etc/yum.repos.d/docker-ce.repo http://172.19.30.116/mirror/.help/docker-ce.repo"
+    ssh root@${node_ip} "wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo"
     ssh root@${node_ip} "yum install -y yum-utils device-mapper-persistent-data lvm2; yum makecache; yum -y install docker-ce; chkconfig docker on"
     ssh root@${node_ip} "mkdir /etc/systemd/system/docker.service.d"
     ssh root@${node_ip} "mkdir -p /etc/docker/; "
-    ssh root@${node_ip} "rm -rf /etc/docker/certs.d/harbor.tophc.top/*;" 
-    ssh root@${node_ip} "rm -rf /etc/docker/certs.d/docker-hub.cloud.top/* ;"
-    ssh root@${node_ip} "mkdir -p /etc/docker/certs.d/harbor.tophc.top" 
-    ssh root@${node_ip} "mkdir -p /etc/docker/certs.d/docker-hub.cloud.top"
+    ssh root@${node_ip} "mkdir -p /srv/data"
     ssh root@${node_ip} "rm -rf /var/lib/docker"
     ssh root@${node_ip} "rm -rf /srv/data/docker"
     scp ./conf/docker.service root@${node_ip}:/usr/lib/systemd/system/docker.service 
     scp ./conf/daemon.json root@${node_ip}:/etc/docker/daemon.json
-    scp ./conf/http-proxy.conf root@${node_ip}:/etc/systemd/system/docker.service.d/http-proxy.conf
-    scp ./conf/harbor.tophc.top.crt root@${node_ip}:/etc/docker/certs.d/harbor.tophc.top/harbor.crt
-    scp ./conf/docker-hub.cloud.top.crt root@${node_ip}:/etc/docker/certs.d/docker-hub.cloud.top/harbor.crt
+    # 代理配置暂时忽略
+    #scp ./conf/http-proxy.conf root@${node_ip}:/etc/systemd/system/docker.service.d/http-proxy.conf
     ssh root@${node_ip} "systemctl daemon-reload; systemctl stop docker; systemctl restart docker; docker info"
   done
 
